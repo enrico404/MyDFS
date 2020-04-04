f6a4501c8ac951a704a8b0460f07ddc331f8fd11
enrico404 <sedoni.enrico@gmail.com>
2020-04-04 12:26:23 +0200

    reinstall modified

--- installation/reinstall.sh
+++ installation/reinstall.sh
@@ -2,4 +2,5 @@
 
 sudo cp -r ../../MyDFS /home/myDfsUser/
 sudo chown -R myDfsUser:myDfsUser /home/myDfsUser/MyDFS
+echo "Reinstallazione completata con successso!"
 exit
--- installation/serverSetup.sh
+++ installation/serverSetup.sh
@@ -4,5 +4,7 @@ sudo adduser --force-badname myDfsUser
 sudo usermod -aG sudo myDfsUser
 sudo cp -r ../../MyDFS /home/myDfsUser/
 sudo chown -R myDfsUser:myDfsUser /home/myDfsUser/MyDFS
+echo "Setting del nuovo utente 'myDfsUser'...."
 su myDfsUser -c 'mkdir -p /home/myDfsUser/shDir'
+echo "Installazione completata con successo!"
 exit
